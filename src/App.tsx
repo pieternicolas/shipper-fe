2b2edf91cc57a8b42772a6720902eb13e9da4cbf
Nicolas Pieter <nicolaspieter@yahoo.com>
2021-07-31 23:05:57 +0700

    Add css and breakpoints

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { lazy, Suspense } from 'react';
+import { css, Global } from '@emotion/react';
+
+const Home = lazy(() => import('pages/Home'));
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Global
+        styles={css`
+          html,
+          body,
+          #root {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            width: 100%;
+          }
+
+          body {
+            font-family: Arial, Helvetica, sans-serif;
+          }
+        `}
+      />
+
+      <Suspense fallback={null}>
+        <Home />
+      </Suspense>
+    </>
+  );
 };
 
 export default App;
---
+++ src/config/styles.ts
@@ -0,0 +1,5 @@
+export enum Breakpoints {
+  Desktop = 768,
+}
+
+export const bpDesktop = `@media only screen and (min-width: ${Breakpoints.Desktop}px)`;
